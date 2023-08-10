ccf20b6400267ee29ea455eb9b74bc3d94ee7cce
samiracode <samiratavakolli.k@gmail.com>
2023-08-10 10:29:16 +0200

    adding loader

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
 
+// Show Loading 
+function loading () {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading 
+function complete () {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 // Show New Quote
 function newQuote () {
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if Authotr quote is blank and replace it with "Anonymouse"
@@ -23,11 +37,14 @@ function newQuote () {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;  
+    complete();
 }
 
 // Get Quotes From API
 async function getQuotes () {
+    loading();
    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'; 
    try {
      const response = await fetch(apiUrl); 
