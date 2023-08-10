bb73d9efbd99d988c4292bdd5e15aeb2249adac3
samiracode <samiratavakolli.k@gmail.com>
2023-08-10 09:57:10 +0200

    Changing long quotes class on DOM

---
+++ script.js
@@ -0,0 +1,52 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote () {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check if Authotr quote is blank and replace it with "Anonymouse"
+    if (!quote.author) {
+        authorText.textContent = "Anonymouse"
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check the quote length to determine styling
+    if (quote.text.length > 120 ) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;  
+}
+
+// Get Quotes From API
+async function getQuotes () {
+   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'; 
+   try {
+     const response = await fetch(apiUrl); 
+     apiQuotes = await response.json();
+     newQuote();
+   } catch (error) {
+    //here we handle errors!
+   }
+}
+
+// On Load 
+getQuotes();
+
+
+
+
+
+
+    
+
+   
+   
